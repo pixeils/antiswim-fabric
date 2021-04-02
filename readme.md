5355087b263d11f285350fd6a1e09c7a73a87a08
pixeils <79124407+pixeils@users.noreply.github.com>
2021-04-03 02:23:46 +1100

    fixed multiplayer

---
+++ readme.md
@@ -0,0 +1,14 @@
+antiswim-fabric
+
+Press "]" to enable or disable AntiSwim. Keybinding can be changed in vanilla control menu.
+
+If you are in Swim Mode and enable AntiSwim you will stay in Swim Mode while in water, 
+when you leave the water you will leave swim mode but next time you go in the water you will not be able to swim
+until you toggle AntiSwim.
+
+If you are not in Swim Mode but in water and enable AntiSwim you will not be able to enter swim mode, 
+however you will not be able to sprint.
+
+This is useful for digging underwater ect.
+
+Requires Fabric API.
--- src/main/java/net/pixeils/antiswim/mixin/EntityMixin.java
+++ src/main/java/net/pixeils/antiswim/mixin/EntityMixin.java
@@ -3,10 +3,25 @@ package net.pixeils.antiswim.mixin;
 import net.minecraft.entity.Entity;
 import net.pixeils.antiswim.client.AntiSwim;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+
+    @Shadow public abstract boolean isSubmergedInWater();
+
+    @Inject(method = "setSprinting",at = @At("HEAD"),cancellable = true)
+    public void setSprinting(CallbackInfo ci) {
+        if (!AntiSwim.antiswim && isSubmergedInWater())
+            ci.cancel();
+
+    }
+}
+/*
 @Mixin(Entity.class)
 public class EntityMixin {
 
@@ -17,3 +32,4 @@ public class EntityMixin {
 
     }
 }
+*/
